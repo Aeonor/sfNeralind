311e67ba5f6833c1ec4a619d9d4900974cb1a5cc
aurelienmagne <aurelienmagne@macbookpro-ngc.local>
2014-02-25 12:05:03 +0100

    Init du projet, install de FOSUserBundle

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-sfNeralind
-==========
+NERALIND
---
+++ src/Neralind/UserBundle/NeralindUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Neralind\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NeralindUserBundle extends Bundle
+{
+}
