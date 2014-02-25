c7380daee337b81497332c5d533d187e0b713b9d
aurelienmagne <aurelienmagne@10.0.100.45>
2014-02-25 15:19:15 +0100

    Ajout de STOF Doctrine Extensions

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Neralind\UserBundle\NeralindUserBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
